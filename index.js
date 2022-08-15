852a0d5e163a31c1204473bfe52a467276122a5a
Aypixels <83863116+Aypixels@users.noreply.github.com>
2022-08-15 14:00:51 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+let date = Date.parse('2022-11-12 18:00:10')
+let today = Date.now()
+
+function convert(date1, date2) {
+    let days = Math.floor(((date - today) / 1000 / 60 / 60 / 24));
+    let hours = Math.floor(((date - today) / 1000 / 60 / 60) % 24);
+    let minutes = Math.floor(((date - today) / 1000 / 60) % 60);
+    let seconds = Math.floor(((date - today) / 1000) % 60);
+
+    return {
+        days,
+        hours, 
+        minutes, 
+        seconds
+    }
+} 
+
+console.log(convert(date, today))
