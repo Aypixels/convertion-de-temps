014e19f9ece4085557e9c67135b68f9b9b312cc3
Aypixels <83863116+Aypixels@users.noreply.github.com>
2022-08-19 04:39:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
 from datetime import datetime
 from math import floor
-import sched, time 
 
-s = sched.scheduler(time.time, time.sleep) 
+date1 = datetime.now()
+date2 = datetime(2022, 8, 20, 0, 0, 0, 0)
 
-def do_something(sc): 
+def convert (date1, date2):
 
-    date1 = datetime.now()
-    date2 = datetime(2022, 8, 20, 0, 0, 0, 0)
+    diff = date2 - date1
 
-    def convert (date1, date2):
+    day = floor(diff.seconds / 60 / 60 / 24)
+    hour = floor((diff.seconds / 60 / 60) % 24)
+    minutes = floor((diff.seconds / 60) % 60)
+    seconds = floor((diff.seconds) % 60)
 
-        diff = date2 - date1
+    return {'days': day, 'hours': hour, 'minutes': minutes, 'seconds': seconds}
 
-        day = floor(diff.seconds / 60 / 60 / 24)
-        hour = floor((diff.seconds / 60 / 60) % 24)
-        minutes = floor((diff.seconds / 60) % 60)
-        seconds = floor((diff.seconds) % 60)
+convertion = convert(date1, date2)
 
-        return {'days': day, 'hours': hour, 'minutes': minutes, 'seconds': seconds}
-
-    convertion = convert(date1, date2)
-
-    print(convertion.get('days'), 'jours\n', convertion.get('hours'), 'heures\n', convertion.get("minutes"), 'minutes &', convertion.get('seconds'), 'secondes')
-    # do your stuff 
-    sc.enter(1, 1, do_something, (sc,)) 
-
-s.enter(1, 1, do_something, (s,))
-s.run()
-
-
-
-# print(convertion.get('days'), 'jours\n', convertion.get('hours'), 'heures\n', convertion.get("minutes"), 'minutes &', convertion.get('seconds'), 'secondes')
+print(convertion.get('days'), 'jours\n', convertion.get('hours'), 'heures\n', convertion.get("minutes"), 'minutes &', convertion.get('seconds'), 'secondes')
